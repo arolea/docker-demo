3c1baa0af064049fd797fccb316d1398d9480211
Alex Rolea <alexandru.rolea@softvision.com>
2020-07-13 15:41:24 +0300

    Brancj with no DB dependencies

--- src/main/java/com/rolea/learning/dockerlearning/DockerLearningApplication.java
+++ src/main/java/com/rolea/learning/dockerlearning/DockerLearningApplication.java
@@ -1,26 +1,10 @@
 package com.rolea.learning.dockerlearning;
 
-import com.rolea.learning.dockerlearning.domain.Person;
-import com.rolea.learning.dockerlearning.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerLearningApplication implements CommandLineRunner {
-
-	@Autowired
-	private PersonService personService;
-
-	@Override
-	public void run(String... args) {
-		if(personService.count() == 0) {
-			Person person = new Person();
-			person.setName("Alex");
-			personService.save(person);
-		}
-	}
+public class DockerLearningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerLearningApplication.class, args);
--- src/main/java/com/rolea/learning/dockerlearning/controller/PersonController.java
+++ src/main/java/com/rolea/learning/dockerlearning/controller/PersonController.java
@@ -1,19 +1,17 @@
 package com.rolea.learning.dockerlearning.controller;
 
-import com.rolea.learning.dockerlearning.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 @RestController
 public class PersonController {
 
-    @Autowired
-    private PersonService personService;
-
     @GetMapping(value = "/hello")
-    public String sayHello(){
-        return String.format("Hello %s!", personService.findByName("Alex").getName());
+    public String sayHello() throws UnknownHostException {
+        return String.format("Hello from %s!", Inet4Address.getLocalHost().getHostAddress());
     }
 
 }
