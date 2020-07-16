6273477d2d6f78f038b3517cfded5dde1dc8abb5
Alex Rolea <alexandru.rolea@softvision.com>
2020-07-16 15:21:27 +0300

    Small changes

--- src/main/java/com/rolea/learning/dockerlearning/controller/PersonController.java
+++ src/main/java/com/rolea/learning/dockerlearning/controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.rolea.learning.dockerlearning.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,12 @@ import java.net.UnknownHostException;
 @RestController
 public class PersonController {
 
+    @Value("${secret.key}")
+    private String secretValue;
+
     @GetMapping(value = "/hello")
     public String sayHello() throws UnknownHostException {
-        return String.format("Hello from %s!", Inet4Address.getLocalHost().getHostAddress());
+        return String.format("Hello from %s! Secret value: %s", Inet4Address.getLocalHost().getHostAddress(), secretValue);
     }
 
 }
