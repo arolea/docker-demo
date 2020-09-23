aebf3e352d054aa86a9cf7762452167d1a5df159
Alex Rolea <alexandru.rolea@softvision.com>
2020-09-23 13:14:26 +0300

    Adjustments for demo

--- kubernetes/README.md
+++ kubernetes/README.md
@@ -12,3 +12,15 @@
 
 ## Kubectl commands ##
 
+## Demo ##
+
+- Build the project and create a fat jar: `./mvnw clean package`
+- Build and push the docker image (see the parent README)
+- Delete minikube to purge any existing cluster state: `minikube delete`
+- Start minikube: `minikube start`
+- Start the minikube dashboard to visualize the cluster state: `minikube dashboard`
+- Create the Kubernetes objects: `kubectl apply -f ./hello-world-config.yml` and `kubectl apply -f ./hello-world-db.yml` and `kubectl apply -f ./hello-world-app.yml`
+- Get the minikube IP: `minikube ip`
+- Get the ports on which services are exposed: `kubectl get services`
+- Optional: Connect to the DB 
+- Invoke the deployed service: `curl --location --request GET 'http://172.17.0.3:31296/hello'`
--- src/main/java/com/rolea/learning/dockerlearning/controller/PersonController.java
+++ src/main/java/com/rolea/learning/dockerlearning/controller/PersonController.java
@@ -2,6 +2,7 @@ package com.rolea.learning.dockerlearning.controller;
 
 import com.rolea.learning.dockerlearning.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,16 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @Value("${app.name:default}")
+    private String appName;
+
+    @Value("${pod.name:none}")
+    private String podName;
+
     @GetMapping(value = "/hello")
     public String sayHello(){
-        return String.format("Hello %s!", personService.findByName("Alex").getName());
+        return String.format("Hello %s from app %s and pod %s!",
+                personService.findByName("Alex").getName(), appName, podName);
     }
 
 }
